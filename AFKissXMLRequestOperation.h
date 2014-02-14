7e4b4f73aa5ba65b65400b281ffba2396b1d1342
Alex Manarpies <alex@manarpies.com>
2014-02-14 14:41:45 +0100

    Add static library project

--- AFKissXMLRequestOperation.h
+++ AFKissXMLRequestOperation.h
@@ -20,8 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
-#import "AFHTTPRequestOperation.h"
+#import <AFHTTPRequestOperation.h>
 
 @class DDXMLDocument;
 
---
+++ AFKissXMLRequestOperationLib/AFKissXMLRequestOperationLibTests/AFKissXMLRequestOperationLibTests.m
@@ -0,0 +1,34 @@
+//
+//  AFKissXMLRequestOperationLibTests.m
+//  AFKissXMLRequestOperationLibTests
+//
+//  Created by Alex Manarpies on 14/02/14.
+//  Copyright (c) 2014 AFNetworking. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFKissXMLRequestOperationLibTests : XCTestCase
+
+@end
+
+@implementation AFKissXMLRequestOperationLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
